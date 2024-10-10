7c3001cd3249830cf2d7ce7a5c79e2ce10e095a4
mohammed <mohammedbadranm5@gmail.com>
2024-10-10 13:32:34 +0300

    Add project files.

---
+++ studentportal/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using studentportal.Models.Entities;
+
+namespace studentportal.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)//save it in you mind
+        {
+            
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
